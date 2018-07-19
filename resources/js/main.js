39df46cc8a250f1836a3b79064bdc4790b7f160c
seXy <matthew.louis@stu.spencer.kyschools.us>
2018-07-19 16:44:16 -0400

    first banner pretty much done

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,18 +1,22 @@
 $(document).ready(function(){
   $('.aa').mouseenter(function (){
-    $(this).children().animate({'width':'45%'}, 100);
-    $(this).animate({'width':'90%'}, 100);
+    $(this).animate({'width':'90%'}, 1000);
+    $(this).children('.fa').delay(670).fadeIn();
+    $(this).children('.fa').delay(1000).css('margin-right','40%');
   });
   $('.aa').mouseleave(function () {
-    $(this).children().animate({'width':'100%'}, 100);
-    $(this).animate({'width':'40%'}, 100);
+    $(this).animate({'width':'40%'}, 1000);
+    $(this).children('.fa').css('display','none');
+    $(this).children('.fa').css('margin-right','0');
   });
   $('.aal').mouseenter(function (){
-    $(this).children().animate({'width':'45%'}, 100);
-    $(this).animate({'width':'90%'}, 100);
+    $(this).animate({'width':'90%'}, 1000);
+    $(this).children('.fa').delay(670).fadeIn();
+    $(this).children('.fa').delay(1000).css('margin-left','40%');
   });
   $('.aal').mouseleave(function () {
-    $(this).children().animate({'width':'100%'}, 100);
-    $(this).animate({'width':'40%'}, 100);
+    $(this).animate({'width':'40%'}, 1000);
+    $(this).children('.fa').css('display','none');
+    $(this).children('.fa').css('margin-right','0');
   });
 });
