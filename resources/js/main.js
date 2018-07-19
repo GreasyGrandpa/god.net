4afef5ea2f8906e5516b587429e100fefbd93178
seXy <matthew.louis@stu.spencer.kyschools.us>
2018-07-19 14:01:05 -0400

    halfway doneish

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
-  $('p').css('color', 'red');
-  $('p').click(function(){
-    $(this).append(' baby');
+  $('.a').mouseenter(function (){
+    $(this).animate({'padding-left':'50%'}, 1000);
+    });
+  $('.a').mouseleave(function () {
+    $(this).animate({'padding-left':'0'}, 1000)
+  });
+  $('.al').mouseenter(function (){
+    $(this).animate({'padding-right':'50%'}, 1000);
+    });
+  $('.al').mouseleave(function () {
+    $(this).animate({'padding-right':'0'}, 1000)
   });
 });
