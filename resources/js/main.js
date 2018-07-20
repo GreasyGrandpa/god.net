6e6c72139e67c56f468199bf4341a844fcf07590
seXy <matthew.louis@stu.spencer.kyschools.us>
2018-07-20 14:34:18 -0400

    finally got the buttons debugged

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,20 +1,26 @@
 $(document).ready(function(){
   $('.aa').mouseenter(function (){
+    $(this).children('.fa').finish();
+    $(this).finish();
     $(this).animate({'width':'90%'}, 1000);
-    $(this).children('.fa').delay(670).fadeIn();
-    $(this).children('.fa').delay(1000).css('margin-right','40%');
+    $(this).children('.fa').delay(1000).fadeIn();
+    $(this).children('.fa').delay(1500).css('margin-right','40%');
   });
   $('.aa').mouseleave(function () {
+    $(this).children('.fa').finish();
     $(this).animate({'width':'40%'}, 1000);
     $(this).children('.fa').css('display','none');
     $(this).children('.fa').css('margin-right','0');
   });
   $('.aal').mouseenter(function (){
+    $(this).children('.fa').finish();
+    $(this).finish();
     $(this).animate({'width':'90%'}, 1000);
-    $(this).children('.fa').delay(670).fadeIn();
-    $(this).children('.fa').delay(1000).css('margin-left','40%');
+    $(this).children('.fa').delay(1000).fadeIn();
+    $(this).children('.fa').delay(1500).css('margin-left','40%');
   });
   $('.aal').mouseleave(function () {
+    $(this).children('.fa').finish();
     $(this).animate({'width':'40%'}, 1000);
     $(this).children('.fa').css('display','none');
     $(this).children('.fa').css('margin-right','0');
