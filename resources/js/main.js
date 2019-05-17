bde5b4e496de77407622f35e8eb91245ae57b574
seXy <matthew.louis@stu.spencer.kyschools.us>
2019-05-17 02:37:06 -0400

    hamilton modification

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+  //moving buttons on first banner
   $('.aa').mouseenter(function (){
     $(this).children('.fa').finish();
     $(this).finish();
@@ -25,4 +26,36 @@ $(document).ready(function(){
     $(this).children('.fa').css('display','none');
     $(this).children('.fa').css('margin-right','0');
   });
+  $('.button').mouseenter(function(){
+    $(this).children().finish();
+    $(this).children().animate({'font-weight':'bold','font-size':'1.5rem'}, 200);
+  });
+  $('.button').mouseleave(function(){
+    $(this).children().finish();
+    $(this).children().animate({'font-weight':'normal','font-size':'1rem'}, 200);
+  });
+  //weather button
+  $('.button').click(function(){
+    var state = $('.state-input').val();
+    var city = $('.city-input').val();
+    var url = 'https://api.wunderground.com/api/4e1fe0d302980707/geolookup/conditions/q/' + state + '/' + city + '.json';
+    $.ajax({
+     url: url,
+     datatype: 'jsonp',
+     success: function(data){
+       $('.righta .conditiona .value').html('<p class="data">' + data.current_observation.weather + '</p>');
+       $('.righta .temp .value').html('<p class="data">' + data.current_observation.temp_f + '</p>')
+       $('.righta .humidity .value').html('<p class="data">' + data.current_observation.relative_humidity + '</p>')
+     }});
+     var weather = $('.data').val();
+     $('.conditiona').fadeIn();
+     $('.conditiona').css('display','inline-flex');
+     $('.temp').fadeIn();
+     $('.temp').css('display','inline-flex');
+     $('.humidity').fadeIn();
+     $('.humidity').css('display','inline-flex');
+     $('.state-input').val('');
+     $('.city-input').val('');
+  });
+  //second button
 });
